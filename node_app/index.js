1a968623175f912ec4a7c050c20614e1e1911ff3
Barbara Cristina <babisantos1703@gmail.com>
2025-09-16 20:25:15 -0300

    PROJETO - DOCKER

--- node_app/index.js
+++ node_app/index.js
@@ -12,19 +12,67 @@ const dbConfig = {
   database: "appdb"
 };
 
-app.get("/", (req, res) => {
-  res.json({ message: "Node.js está rodando no Docker!" });
-});
-
-app.get("/users", async (req, res) => {
-  try {
+app.get("/api/v1/cliente",async (req, res) => {
+  //res.json({ message: "Hello World!" });
+  try{
     const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.execute("SELECT * FROM users");
+    const [rows] = await connection.execute("SELECT * FROM clientes");
     await connection.end();
     res.json(rows);
+  } catch(err){
+    res.status(500).json({error:err.message});
+  }
+});
+
+app.get("/api/v1/cliente/:id", async (req, res) => {
+  try {
+    const cliente = req.params.id;
+    const connection = await mysql.createConnection(dbconfig);
+    const [rows] = await connection.execute("SELECT * FROM clientes where id = ?",[cliente]);
+    await connection.destroy();
+    res.json(rows); 
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.post("/api/v1/cliente/",async(req,res)=>{
+
+  try{
+    const cliente = req.body;
+
+    if(cliente.email == null || cliente.nome == null || cliente.celular == null){
+      res.statusCode(500).json({error:"Faltou algum parametro"})
+    } 
+    const connection = await mysql.createConnection(dbConfig);
+    const [result] = await connection.execute("insert into clientes(email,telefone,nome) values(?,?,?",[cliente.e]);
+    await connection.end();
+
+    if(result.affectedRows > 0){
+      res.json({
+        message:"Cliente inserido com sucesso!",
+        id:result.insertId
+      });
+    } else{
+      res.statusCode(500).json({error:"Falha ao inserir cliente."});
+    }
+  } catch(err){
+    res.status(500).json({error:err.message});
+  }
+});
+
+app.delete("/api/v1/cliente/:id",async(req,res)=>{
+    try{
+      const cliente = req.params.id;
+
+      const connection = await mysql.createConnection(dbconfig);
+      const [rows] = await connection.execute("delete from clientes where id = ?",[cliente]);
+      await connection.end();
+      res.json(rows);
+    } catch (err){
+      res.status(500).json({error:err.message});
+    }
+
 });
 
 app.listen(PORT, () => {
